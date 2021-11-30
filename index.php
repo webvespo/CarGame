00edad6ecc4f344c1a196c1ced64f89d7dff244a
webvespo <82005304+webvespo@users.noreply.github.com>
2021-11-30 00:11:47 -0300

    Update index.php
    Subir 30-11-2021

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Inicio del Site</title>
 </head>
 <body>
-    <?php
+<?php
     include './controladores/modelos.php';
     if(!$_GET){
             include 'nav.php';
@@ -17,7 +17,26 @@
             include 'nav.php';
             echo  htmlspecialchars($_GET['modelo']);
                 }
-       
 ?>
+    <div class="row">
+    <div class="card" style="width: 18rem;">
+  <img src=".\imagenes\chevrolet_impala_1967.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Chevrolet Impala 1967</h5>
+    <p class="card-text">Exclusiva gama de autos de colección, para clientes con gusto vintage. Podrias quedar bien con tu abuelo!.</p>
+    <a href="#" class="btn btn-primary">Ir a Autos De Colección</a>
+  </div>
+  <br>
+</div>
+<div class="card" style="width: 18rem;">
+  <img src=".\imagenes\chevrolet_impala_1967.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Chevrolet Impala 1967</h5>
+    <p class="card-text">Exclusiva gama de autos de colección, para clientes con gusto vintage. Podrias quedar bien con tu abuelo!.</p>
+    <a href="#" class="btn btn-primary">Ir a Autos De Colección</a>
+  </div>
+</div>    
+</div> 
+
 </body>
 </html>
